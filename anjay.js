b7032f54623a9ccf68ba743b76dc9febe8f5a48d
fauzigaga <fauzigaga59@gmail.com>
2025-01-04 20:59:17 +0700

    first commit

---
+++ anjay.js
@@ -0,0 +1,57 @@
+const endpoint = "http://192.168.0.108";
+
+function getEspLed(){
+    fetch(endpoint + "/led1",{
+        method:"GET"
+    }).then(response => response.text()).then(result => {
+        if(result == "ON"){
+            ledEsp.style.backgroundColor = "yellow";
+            lampuOn.src="./assets/lampuOn.jpg"
+        }else {
+            ledEsp.style.backgroundColor = "white";
+        }
+});
+}
+function getEspLed2(){
+    fetch(endpoint + "/led2",{
+        method:"GET"
+    }).then(response => response.text()).then(result => {
+        if(result == "ON"){
+            ledEsp2.style.backgroundColor = "red";
+        }else {
+            ledEsp2.style.backgroundColor = "white";
+        }
+});
+}
+// function getEspLed3(){
+//     fetch(endpoint + "/led3",{
+//         method:"GET"
+//     }).then(response => response.text()).then(result => {
+//         if(result == "ON"){
+//             ledEsp3.style.backgroundColor = "red";
+//         }else {
+//             ledEsp3.style.backgroundColor = "blue";
+//         }
+
+// });
+// }
+function setEspLed(){
+    fetch(endpoint + "/led1",{
+        method:"POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+function setEspLed2(){
+    fetch(endpoint + "/led2",{
+        method:"POST"
+    }).then(response => response.text()).then(() => location.reload());
+}
+// function setEspLed3(){
+//     fetch(endpoint + "/led3",{
+//         method:"POST"
+//     }).then(response => response.text()).then(() => location.reload());
+// }
+
+
+getEspLed();
+getEspLed2();
+// getEspLed3();
