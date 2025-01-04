cf1b63dd5a76366576358aa13da6b8635f91e162
fauzigaga <fauzigaga59@gmail.com>
2025-01-04 22:08:54 +0700

    change

--- anjay.js
+++ anjay.js
@@ -1,27 +1,34 @@
-const endpoint = "https://192.168.0.108";
+const endpoint = "http://192.168.0.108";
 
-function getEspLed(){
-    fetch(endpoint + "/led1",{
-        method:"GET"
-    }).then(response => response.text()).then(result => {
-        if(result == "ON"){
-            ledEsp.style.backgroundColor = "yellow";
-        }else {
-            ledEsp.style.backgroundColor = "white";
-        }
-});
-}
-function getEspLed2(){
-    fetch(endpoint + "/led2",{
-        method:"GET"
-    }).then(response => response.text()).then(result => {
-        if(result == "ON"){
-            ledEsp2.style.backgroundColor = "red";
-        }else {
-            ledEsp2.style.backgroundColor = "white";
-        }
-});
-}
+async function getEspLed() {
+    try {
+      const response = await fetch(endpoint + "/led1", { method: "GET" });
+      const result = await response.text();
+      if (result === "ON") {
+        ledEsp.style.backgroundColor = "yellow";
+      } else {
+        ledEsp.style.backgroundColor = "white";
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  async function getEspLed2() {
+    try {
+      const response = await fetch(endpoint + "/led2", { method: "GET" });
+      const result = await response.text();
+      if (result === "ON") {
+        ledEsp2.style.backgroundColor = "red";
+      } else {
+        ledEsp2.style.backgroundColor = "white";
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  
 // function getEspLed3(){
 //     fetch(endpoint + "/led3",{
 //         method:"GET"
@@ -34,16 +41,24 @@ function getEspLed2(){
 
 // });
 // }
-function setEspLed(){
-    fetch(endpoint + "/led1",{
-        method:"POST"
-    }).then(response => response.text()).then(() => location.reload());
-}
-function setEspLed2(){
-    fetch(endpoint + "/led2",{
-        method:"POST"
-    }).then(response => response.text()).then(() => location.reload());
-}
+async function setEspLed() {
+    try {
+      await fetch(endpoint + "/led1", { method: "POST" });
+      getEspLed(); // Update status LED
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  async function setEspLed2() {
+    try {
+      await fetch(endpoint + "/led2", { method: "POST" });
+      getEspLed2(); // Update status LED
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
 // function setEspLed3(){
 //     fetch(endpoint + "/led3",{
 //         method:"POST"
